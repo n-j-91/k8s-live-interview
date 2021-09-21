cfd665a88e30e5f5c505e27180e51f3ca0ed85f6
NJ <nayanajith.chandradasa@neueda.com>
2021-09-21 23:06:08 +0100

    readsecret method added

--- app/sample-app/service/controller.py
+++ app/sample-app/service/controller.py
@@ -16,12 +16,29 @@ def health_check():
     data = {"msg": "Server is healthy", "status_code": 200}
     return jsonify(data), 200
 
+
+@app.route("/v1/readsecret", methods=["GET"])
+def read_secret():
+    """
+    Requests sent to /v1/readsecret (root) are handled by this method.
+    :return: A secret read from MY_SECRET environment variable
+    """
+    data = None
+    if settings.MY_SECRET is not None:
+        logging.debug("secret, {0} is available in MY_SECRET environment variable".format(settings.MY_SECRET))
+        data = {"msg": "MY_SECRET is {0}".format(settings.MY_SECRET), "status_code": 200}
+        return jsonify(data), 200
+    else:
+        logging.error("secret is not available in MY_SECRET environment variable")
+        data = {"msg": "MY_SECRET is not found in environment variables", "status_code": 404}
+        return jsonify(data), 404
+
+
 @app.route("/v1/readjson", methods=["GET"])
 def read_json():
     """
-    Requests sent to / (root) are handled by this method.
-    Considered as a health check endpoint for the application.
-    :return: A tuple with json data and status_code
+    Requests sent to /v1/readjson (root) are handled by this method.
+    :return: A list of all json content loaded from the settings.JSON_DIR
     """
     data = jsonreader.read_json()
     if len(data) == 0:
--- app/sample-app/service/settings.py
+++ app/sample-app/service/settings.py
@@ -5,3 +5,4 @@ The configurations can be over-ridden with Environment Variables during run time
 """
 
 JSON_DIR = os.getenv("JSON_DIR", "/usr/src/sample-app/resources")
+MY_SECRET = os.getenv("MY_SECRET", None)
