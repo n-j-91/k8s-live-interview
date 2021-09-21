50fe6bb518110daee4a1687f727a7922bc31d498
NJ <nayanajith.chandradasa@neueda.com>
2021-09-21 20:56:19 +0100

    healthcheck changed/deployment configmaps added

--- app/sample-app/service/controller.py
+++ app/sample-app/service/controller.py
@@ -6,25 +6,23 @@ from . import settings
 app = Flask("receiver")
 
 
-@app.route("/health", methods=["GET"])
+@app.route("/_status/healthz", methods=["GET"])
 def health_check():
     """
     Requests sent to / (root) are handled by this method.
     Considered as a health check endpoint for the application.
     :return: A tuple with json data and status_code
     """
-    logging.debug("GET /health is called")
     data = {"msg": "Server is healthy", "status_code": 200}
     return jsonify(data), 200
 
-@app.route("/readjson", methods=["GET"])
+@app.route("/v1/readjson", methods=["GET"])
 def read_json():
     """
     Requests sent to / (root) are handled by this method.
     Considered as a health check endpoint for the application.
     :return: A tuple with json data and status_code
     """
-    logging.debug("GET /readjson is called")
     data = jsonreader.read_json()
     if len(data) == 0:
         data = {"msg": "No json files found in {0}".format( settings.JSON_DIR), "status_code": 404}
